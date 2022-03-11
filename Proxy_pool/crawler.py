b73183926df72d6266a6db27c5ec12091cb68bb2
guo.chen <guo.chen@qingteng.cn>
2022-03-11 14:19:39 +0800

    新增免费ip代理网站的爬虫：www.89ip.cn

--- Proxy_pool/crawler.py
+++ Proxy_pool/crawler.py
@@ -24,7 +24,7 @@ class Crawler(object, metaclass=ProxyMetaclass):
             proxies.append(proxy)
         return proxies
 
-    def crawl_daili66(self, page_count=4):
+    def crawl_daili66(self, page_count=3):
         """
         获取代理66
         :param page_count: 页码
@@ -33,7 +33,6 @@ class Crawler(object, metaclass=ProxyMetaclass):
         start_url = 'http://www.66ip.cn/{}.html'
         urls = [start_url.format(page) for page in range(1, page_count + 1)]
         for url in urls:
-            print('Crawling', url)
             html = get_page(url)
             if html:
                 doc = pq(html)
@@ -42,29 +41,6 @@ class Crawler(object, metaclass=ProxyMetaclass):
                     ip = tr.find('td:nth-child(1)').text()
                     port = tr.find('td:nth-child(2)').text()
                     yield ':'.join([ip, port])
-
-    # def crawl_xicidaili(self):
-    #     for i in range(1, 3):
-    #         start_url = 'http://www.xicidaili.com/nn/{}'.format(i)
-    #         headers = {
-    #             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    #             'Cookie': '_free_proxy_session=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJWRjYzc5MmM1MTBiMDMzYTUzNTZjNzA4NjBhNWRjZjliBjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMUp6S2tXT3g5a0FCT01ndzlmWWZqRVJNek1WanRuUDBCbTJUN21GMTBKd3M9BjsARg%3D%3D--2a69429cb2115c6a0cc9a86e0ebe2800c0d471b3',
-    #             'Host': 'www.xicidaili.com',
-    #             'Referer': 'http://www.xicidaili.com/nn/3',
-    #             'Upgrade-Insecure-Requests': '1',
-    #         }
-    #         html = get_page(start_url, options=headers)
-    #         if html:
-    #             find_trs = re.compile('<tr class.*?>(.*?)</tr>', re.S)
-    #             trs = find_trs.findall(html)
-    #             for tr in trs:
-    #                 find_ip = re.compile('<td>(\d+\.\d+\.\d+\.\d+)</td>')
-    #                 re_ip_address = find_ip.findall(tr)
-    #                 find_port = re.compile('<td>(\d+)</td>')
-    #                 re_port = find_port.findall(tr)
-    #                 for address, port in zip(re_ip_address, re_port):
-    #                     address_port = address + ':' + port
-    #                     yield address_port.replace(' ', '')
 
     def crawl_ip3366(self):
         for i in range(1, 4):
@@ -94,3 +70,20 @@ class Crawler(object, metaclass=ProxyMetaclass):
                 for address, port in zip(re_ip_address, re_port):
                     address_port = address + ':' + port
                     yield address_port.replace(' ', '')
+
+    def crawl_89ip(self):
+        for i in range(1, 4):
+            start_url = 'https://www.89ip.cn/index_{}.html'.format(i)
+            html = get_page(start_url)
+            if html:
+                find_tr = re.compile('<tr>(.*?)</tr>', re.S)
+                trs = find_tr.findall(html)
+                for index in range(1, len(trs)):
+                    tr = trs[index].replace("\t", "").replace("\n", "").replace(" ", "")
+                    find_ip = re.compile('<td>(\d+\.\d+\.\d+\.\d+)</td>')
+                    re_ip_address = find_ip.findall(tr)
+                    find_port = re.compile('<td>(\d+)</td>')
+                    re_port = find_port.findall(tr)
+                    for address, port in zip(re_ip_address, re_port):
+                        address_port = address + ':' + port
+                        yield address_port.replace(' ', '')
